d04e4b33d945fb8717f9b4c53b3ac56b655a43e0
Arthur Guedes <a2023951334@teiacoltec.org>
2026-09-10 12:03:45 -0300

    versão mais atualizada do jogo até agorakk

--- src/scripts/menu_principal.gd
+++ src/scripts/menu_principal.gd
@@ -13,3 +13,23 @@ func _on_sair_pressed():
 	$"SomDoBotão".play();
 	await get_tree().create_timer(0.5).timeout
 	get_tree().quit() #faça com que o jogo sai.
+
+func _on_label_mouse_entered() -> void:
+	$"Button_manager/Começar/Label".modulate = Color.YELLOW
+
+func _on_label_mouse_exited() -> void:
+	$"Button_manager/Começar/Label".modulate = Color.WHITE
+
+
+func _on_label_opções_mouse_entered() -> void:
+	$"Button_manager/Opções/label_opções".modulate = Color.YELLOW
+	
+func _on_label_opções_mouse_exited() -> void:
+	$"Button_manager/Opções/label_opções".modulate = Color.WHITE
+	
+
+func _on_label_sair_mouse_entered() -> void:
+	$Button_manager/Sair/label_sair.modulate = Color.DARK_RED
+
+func _on_label_sair_mouse_exited() -> void:
+	$Button_manager/Sair/label_sair.modulate = Color.WHITE
